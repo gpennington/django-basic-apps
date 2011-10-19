adbf218023be504e017ef0a20d846415ee02031d
gpennington <garrettp@gmail.com>
2011-10-18 22:04:54 -0500

    Updated setup.py to also include files other than .py Taken from pull request 28 in the original repo by Nathan Borror

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
 
 
 def fullsplit(path, result=None):
@@ -17,10 +18,13 @@ def fullsplit(path, result=None):
     return fullsplit(head, [tail] + result)
 
 
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
 package_dir = "basic"
 
-
 packages = []
+data_files = []
 for dirpath, dirnames, filenames in os.walk(package_dir):
     # ignore dirnames that start with '.'
     for i, dirname in enumerate(dirnames):
@@ -28,11 +32,13 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
             del dirnames[i]
     if "__init__.py" in filenames:
         packages.append(".".join(fullsplit(dirpath)))
-
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(name='django-basic-apps',
     version='0.7',
     description='Django Basic Apps',
     author='Nathan Borror',
     url='http://github.com/nathanborror/django-basic-apps',
-    packages=packages)
+    packages=packages,
+    data_files = data_files,)
